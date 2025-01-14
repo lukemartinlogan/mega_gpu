604ad9390ae9e697c6d7162683fa07bf9f5c51df
lukemartinlogan <lukemartinlogan@gmail.com>
2025-01-14 10:56:55 -0600

    Test pic compile

---
+++ rocm/compile_host_device/test_lib2.cc
@@ -0,0 +1,6 @@
+#include "test_lib2.h"
+
+void TestLib2::Try() {
+  TestLib test_lib;
+  test_lib.GetSize();
+}
---
+++ rocm/compile_host_device/test_lib2.h
@@ -0,0 +1,12 @@
+
+#ifndef TEST_LIB2_H
+#define TEST_LIB2_H
+
+#include "test_lib.h"
+
+class TestLib2 {
+ public:
+  void Try();
+};
+
+#endif  // TEST_LIB2_H
